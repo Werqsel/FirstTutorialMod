c655427ed71352a64adb94e3ab0e8852787ea912
Werqsel <wersal.ilp@gmail.com>
2024-11-10 01:02:52 +0200

    0.1.7 (TravelerSword -> Wind Charge)

--- src/main/java/org/wesal/tutorialmod/item/custom/TravelerSword.java
+++ src/main/java/org/wesal/tutorialmod/item/custom/TravelerSword.java
@@ -5,6 +5,7 @@ import net.minecraft.entity.projectile.WindChargeEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.SwordItem;
 import net.minecraft.item.ToolMaterial;
+import net.minecraft.item.tooltip.TooltipType;
 import net.minecraft.text.Text;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
@@ -12,6 +13,9 @@ import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 
 
+import java.util.List;
+
+import static java.lang.Math.*;
 import static net.minecraft.util.math.Direction.*;
 
 public class TravelerSword extends SwordItem {
@@ -21,6 +25,16 @@ public class TravelerSword extends SwordItem {
 
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        for (double x = -2; x >= -2 && x <= 2; x = x+0.1){
+            double y = sqrt(abs(pow(2,2) - pow(x,2)));
+            WindChargeEntity windChargeEntity = new WindChargeEntity(user, world, user.getPos().getX() + x, user.getEyePos().getY(), user.getPos().getZ() + y);
+            windChargeEntity.setVelocity(user, user.getPitch(), user.getYaw(), 0.0F, 1.5F, 1.0F);
+            world.spawnEntity(windChargeEntity);
+//            y = -(y);
+//            windChargeEntity = new WindChargeEntity(user, world, user.getPos().getX() + x, user.getEyePos().getY(), user.getPos().getZ() + y);
+//            windChargeEntity.setVelocity(user, user.getPitch(), user.getYaw(), 0.0F, 1.5F, 1.0F);
+//            world.spawnEntity(windChargeEntity);
+        }
 //        if (user.getFacing().equals(SOUTH) || user.getFacing().equals(NORTH)) {
 //                for (int i = 0; i <= 10; i++) {
 //                WindChargeEntity windChargeEntity = new WindChargeEntity(user, world, user.getPos().getX() + i * 0.5, user.getEyePos().getY(), user.getPos().getZ());
@@ -42,15 +56,12 @@ public class TravelerSword extends SwordItem {
 //            }
 //        }
 //        return super.use(world, user, hand);
-        for (int i = 0; i<10;i++){
-            for (int j = 0; j <10; j++){
-                // x^2+y^2 = R^2
-                // x^2 = R^2 - y^2
-                WindChargeEntity windChargeEntity = new WindChargeEntity(user, world, user.getPos().getX(), user.getEyePos().getY(), user.getPos().getZ());
-                windChargeEntity.setVelocity(user, user.getPitch(), user.getYaw(), 0.0F, 1.5F, 1.0F);
-                world.spawnEntity(windChargeEntity);
-            }
-        }
+
+//        for (double x = -2; x >= -2 && x <= 2; x = x+0.1){
+//            double y = sin(x);
+//            WindChargeEntity windChargeEntity = new WindChargeEntity(user, world, user.getPos().getX() + x, user.getEyePos().getY(), user.getPos().getZ() + y);
+//            world.spawnEntity(windChargeEntity);
+//        }
         return super.use(world, user, hand);
-        }
     }
+}
